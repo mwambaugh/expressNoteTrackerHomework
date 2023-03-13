e46de3f857e35677f63463dbf2112f4d5117bce4
Wambaugh <marwambaugh@gmail.com>
2023-03-13 13:32:47 -0700

    addedroutefiles

---
+++ routes/apiRoute.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const db = require("../db/db.json");
+// const UUID = require("uuid");
+// const router = express.Router();
+
+module.exports = function (app) {
+    app.get('api/notes', (req, res) => {
+        fs.readFile(__dirname, + "../db/db.json", (err, data) => {
+            if (err) throw err;
+            res.json(JSON.parse(data));
+        });
+    });
+    app.post('api/notes', (req, res) => {
+        let noteTotal = [];
+        let newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: UUID.v4(),
+        }
+        fs.readFile(__dirname + '../db/db.json', (err, data) => {
+            if (error) throw err;
+            noteTotal = JSON.parse(data);
+            noteTotal.push(newNote);
+            fs.writeFile(__dirname + '../db/db.json', JSON.stringffy(noteTotal), "utf-8", err => {
+                if (err) throw err;
+                console.log("good job")
+                res.end();
+            })
+        })
+        console.log(created)
+    });
+
+    app.delete('/api/notes/:id', (req, res) => {
+        let noteId = req.params.id;
+        fs.readFile(__dirname + '../db/db.json', (err, data) => {
+            if (err) throw err;
+            let notes = JSON.parse(data);
+            const filterNote = notes.filter(values => values.id != noteId);
+            fs.writeFile(__dirname + '../db/db.json', JSON.stringify(filterNote), "utf-8", err => {
+                if (err) throw err;
+                res.end();
+            });
+        });
+    });
+};
+
+// module.exports = router;
---
+++ routes/htmlRoute.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const fs = require("fs");
+const express = require("express");
+const router = express.Router();
+
+//Routes 
+module.exports = function (app) {
+    app.get('./public/notes.html', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'))
+    });
+
+    //catch all
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'))
+    });
+};
+
+module.exports = router;
