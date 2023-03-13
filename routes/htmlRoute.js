9c95f14873da198db0ba4d0accefb9b88e3fec90
Wambaugh <marwambaugh@gmail.com>
2023-03-13 14:33:53 -0700

    update to routes

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 //Routes 
 module.exports = function (app) {
-    app.get('/./public/notes.html', (req, res) => {
+    app.get('/./public/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '/../public/notes.html'))
     });
 
