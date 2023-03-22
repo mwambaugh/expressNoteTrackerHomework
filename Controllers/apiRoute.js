075588421f0e8b7847e73924a779f99eb9001385
Wambaugh <marwambaugh@gmail.com>
2023-03-22 12:43:58 -0700

    addedtestData

--- Controllers/apiRoute.js
+++ Controllers/apiRoute.js
@@ -1,46 +1,39 @@
-const fs = require('fs');
-const UUID = require("uuid");
-const db = require("./../db/db.json");
 
+const path = require("path");
+const router = require("express").Router();
+const fs = require("fs");
+const db = require("../../db/db.json");
+const { resolve } = require("path");
 
-module.exports = function (app) {
-    app.get('/api/notes', function (req, res) {
-        fs.readFile(__dirname + "./../db/db.json", (err, data) => {
-            if (err) throw err;
-            res.json(JSON.parse(data));
-        });
-    });
-    app.post('/api/notes', function (req, res) {
-        let noteTotal = [];
-        let newNote = {
-            title: req.body.title,
-            text: req.body.text,
-            id: UUID.v4(),
-        }
-        fs.readFile(__dirname + './../db/db.json', JSON.stringify(allNotes), "utf-8", (err) => {
-            if (error) throw err;
-            noteTotal = JSON.parse(data);
-            noteTotal.push(newNote);
-            fs.writeFile(__dirname + './../db/db.json', JSON.stringify(noteTotal), "utf-8", err => {
-                if (err) throw err;
-                console.log("saved")
-                res.end();
-            })
-        })
-        console.log("Note created");
-    });
+const dbPath = "db/db.json";
 
-    app.delete('/api/notes/:id', (req, res) => {
-        let noteId = req.params.id;
-        fs.readFile(__dirname + './../db/db.json', (err, data) => {
-            if (err) throw err;
-            let notes = JSON.parse(data);
-            const filterNote = notes.filter(values => values.id != noteId);
-            fs.writeFile(__dirname + './../db/db.json', JSON.stringify(filterNote), "utf-8", err => {
-                if (err) throw err;
-                console.log("The note has been deleted.")
-                res.end();
-            });
-        });
-    });
-};
+router.get("/notes", (req, res) => {
+  const notes = db;
+  res.json(notes);
+});
+
+router.post("/notes", (req, res) => {
+  console.log("post note");
+  console.log(req.body);
+  const savedNotes = db;
+  let newNote = {
+    title: req.body.title,
+    text: req.body.text,
+  };
+  savedNotes.push(newNote);
+  console.log(savedNotes);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(savedNotes),
+    (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(true);
+      }
+    }
+  );
+  res.json(savedNotes);
+});
+
+module.exports = router;
--- Controllers/htmlRoute.js
+++ Controllers/htmlRoute.js
@@ -1,13 +1,17 @@
 const path = require("path");
+const router = require("express").Router();
 
-//Routes 
-module.exports = function (app) {
-    app.get('/notes', function (req, res) {
-        res.sendFile(path.join(__dirname, '/../public/notes.html'))
-    });
+// GET Route for homepage
+router.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
-    //catch all
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/index.html'))
-    });
-};
+router.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+router.get("*", (req, res) => {
+  res.sendFile(path.join(_dirname, "public/index.html"));
+});
+
+module.exports = router;
